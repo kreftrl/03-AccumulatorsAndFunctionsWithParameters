c35681cffcb9e3be7f87f6b3eb0d4a9461da1b98
kreftrl <kreftrl@rose-hulman.edu>
2018-11-29 13:26:09 -0500

    done

--- src/m7_summing.py
+++ src/m7_summing.py
@@ -4,9 +4,10 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Robert Kreft.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
+import math
 
 def main():
     """ Calls the   TEST   functions in this module. """
@@ -17,7 +18,7 @@ def main():
 def run_test_sum_cosines():
     """ Tests the   sum_cosines   function. """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # DONE: 2. Implement this function.
     #   It TESTS the  sum_cosines  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -28,20 +29,39 @@ def run_test_sum_cosines():
     print('--------------------------------------------------')
     print('Testing the   sum_cosines   function:')
     print('--------------------------------------------------')
-
+    # Test 1:
+    expected = 0.13416
+    answer = sum_cosines(3)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+    # Test 2:
+    expected = -0.23582
+    answer = sum_cosines(5)
+    print('Test 2 expected:', expected)
+    print('       actual:  ', answer)
+    # Test 3:
+    expected = -0.51948
+    answer = sum_cosines(4)
+    print('Test 3 expected:', expected)
+    print('       actual:  ', answer)
 
 def sum_cosines(n):
-    """
-    What comes in:  A non-negative integer n.
-    What goes out:  The sum of the cosines of the integers
-       0, 1, 2, 3, ... n, inclusive, for the given n.
-    Side effects:   None.
-    Example:
-      If n is 3, this function returns
-        cos(0) + cos(1) + cos(2) + cos(3)   which is about 0.13416.
-    """
+    total = 0
+    for k in range(n+1):
+        total = total + math.cos(k)
+
+    return total
+
+    #"""
+    #What goes out:  The sum of the cosines of the integers
+    #   0, 1, 2, 3, ... n, inclusive, for the given n.
+    #Side effects:   None.
+    #Example:
+    #  If n is 3, this function returns
+    #    cos(0) + cos(1) + cos(2) + cos(3)   which is about 0.13416.
+    #"""
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
@@ -53,7 +73,7 @@ def sum_cosines(n):
 def run_test_sum_square_roots():
     """ Tests the   sum_square_roots   function. """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_square_roots  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -64,23 +84,42 @@ def run_test_sum_square_roots():
     print('--------------------------------------------------')
     print('Testing the   sum_square_roots   function:')
     print('--------------------------------------------------')
-
+    # Test 1:
+    expected = 5.86370
+    answer = sum_square_roots(3)
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+    # Test 2:
+    expected = 8.69213
+    answer = sum_square_roots(4)
+    print('Test 2 expected:', expected)
+    print('       actual:  ', answer)
+    # Test 3:
+    expected = 11.854408
+    answer = sum_square_roots(5)
+    print('Test 3 expected:', expected)
+    print('       actual:  ', answer)
 
 def sum_square_roots(n):
-    """
-    What comes in:  A non-negative integer n.
-    What goes out:  The sum of the square roots of the integers
-       2, 4, 6, 8, ... 2n    inclusive, for the given n.
-           So if n is 7, the last term of the sum is
-           the square root of 14 (not 7).
-    Side effects:   None.
-    Example:
-      If n is 5, this function returns
-         sqrt(2) + sqrt(4) + sqrt(6) + sqrt(8) + sqrt(10),
-      which is about 11.854408.
-    """
+    total = 0
+    for k in range(n + 1):
+        total = total + math.sqrt(2*k)
+
+    return total
+    #"""
+    #What comes in:  A non-negative integer n.
+    #What goes out:  The sum of the square roots of the integers
+     #  2, 4, 6, 8, ... 2n    inclusive, for the given n.
+    #       So if n is 7, the last term of the sum is
+    #       the square root of 14 (not 7).
+    #Side effects:   None.
+    #Example:
+    #  If n is 5, this function returns
+    #     sqrt(2) + sqrt(4) + sqrt(6) + sqrt(8) + sqrt(10),
+    #  which is about 11.854408.
+    #"""
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
